008508c6309b78125eee362c5fd2b2370b509603
Bikalpa_ <bikalpapaudel53@gmail.com>
2023-12-23 11:41:34 +0545

    add:/ dependeicies backend

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.get('/about', (req, res) => {
+  res.send('About');
+})
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact</h1>');
+})
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
