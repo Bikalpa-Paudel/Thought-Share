a2485412de9c8a9b43a041617bae68e096b9a668
Bikalpa_ <bikalpapaudel53@gmail.com>
2023-12-23 12:44:42 +0545

    connect database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,35 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
 const app = express();
+
 const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
-app.get('/about', (req, res) => {
-  res.send('About');
-})
-app.get('/contact', (req, res) => {
-  res.send('<h1>Contact</h1>');
-})
+app.get("/about", (req, res) => {
+  res.send("About");
+});
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact</h1>");
+});
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
